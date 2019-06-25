7c075c43a3c30403ffb6bf73cfa1834004bc77f4
Y <y.n.dorny@gmail.com>
2019-06-25 16:46:58 +0200

    clean

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,12 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Yo';
-  // res.cookie('name', 'Yo is cool', { maxAge: 9000000 })
-  if (req.name === 'Yo') {
-    throw Error('That is a stupid name');
-  }
-  next();
-}
-
 exports.homePage = (req, res) => {
   console.log(req.name)
   res.render('index')
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController')
 // Do work here
-router.get('/', storeController.myMiddleware, storeController.homePage)
+router.get('/', storeController.homePage)
 module.exports = router;

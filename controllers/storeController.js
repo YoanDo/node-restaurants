586f05d384f499f13e3d8c77c82ba1c3dfa14ef2
Y <y.n.dorny@gmail.com>
2019-06-28 15:05:49 +0200

    flash messages

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -8,13 +8,12 @@ exports.homePage = (req, res) => {
 }
 
 exports.addStore = (req, res) => {
-  res.render('editStore', { title: '💩 Add store' })
+  res.render('editStore', { title: 'Add store' })
 }
 
 exports.createStore = async (req, res) => {
-  // console.log(req.body)
   // res.json(req.body)
-  const store = new Store(req.body);
-  await store.save(); // wait for the store to be saved
-  res.redirect('/');
+  const store = await (new Store(req.body)).save(); // wait for the store to be saved
+  req.flash('success', `Successfully created ${store.name}. Care to leave a review ?`) // flash infos
+  res.redirect(`/store/${store.slug}`);
 };

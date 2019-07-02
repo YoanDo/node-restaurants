e6dcc21386407d328c061cafaf5ee69df914831b
Y <y.n.dorny@gmail.com>
2019-07-02 17:01:42 +0200

    mongoose update

--- start.js
+++ start.js
@@ -12,6 +12,7 @@ require('dotenv').config({ path: 'variables.env' });
 // Connect to our Database and handle an bad connections
 mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 mongoose.connection.on('error', (err) => {
   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);

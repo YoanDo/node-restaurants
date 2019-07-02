eaddd63c4f6c0579ea6a1ccd3b6d6e42bf861be1
Y <y.n.dorny@gmail.com>
2019-07-02 15:07:40 +0200

    hotfix

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -21,6 +21,6 @@ exports.createStore = async (req, res) => {
 exports.getStores = async (req, res) => {
   // 1 - query the database for a list of all stores
   const stores = await Store.find();
-  console.log(stores)
+  // console.log(stores)
   res.render('stores', { title: 'Stores', stores });
 }

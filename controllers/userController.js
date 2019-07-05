5b0fece9e74b5b855c2a4bb035f8cfaf372b38f3
Y <y.n.dorny@gmail.com>
2019-07-05 23:33:03 +0200

    Save user in DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,6 +37,5 @@ exports.register = async (req, res, next) => {
   const user = new User({ email: req.body.email, name: req.body.name });
   const registerWithPromise = promisify(User.register, User);
   await registerWithPromise(user, req.body.password);
-  res.send('it works!');
   next(); //pass the authcontroller login
 }

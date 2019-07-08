6392d9bad26b1b379a8dff7bf7033f0ee700d1e5
Y <y.n.dorny@gmail.com>
2019-07-08 19:12:42 +0200

    Auth - check if user is logged

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,3 +12,13 @@ exports.logout = (req, res) => {
   req.flash('success', 'You are now logged out 👋');
   res.redirect('/');
 }
+
+exports.isLoggedIn = (req, res, next) => {
+  // 1 - check if the user is authenticated
+  if (req.isAuthenticated()) { // method coming from passport
+    next(); // carry on, the user is logged in
+    return;
+  }
+  req.flash("error", "Oops you must be logged in to do that!");
+  res.redirect('/login');
+}
--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
-router.get('/add', storeController.addStore);
+router.get('/add',
+  authController.isLoggedIn, storeController.addStore);
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
